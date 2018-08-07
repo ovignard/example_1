da1b6920c510a28b57cde91593d01a941831b709
vignard <olivier.vignard@gemalto.com>
2018-08-07 11:31:26 +0200

    qqq

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,9 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
+
+    String someone2 = "Coucou2";
+    
     return String.format("Hello, %s!", someone);
   }
 }
